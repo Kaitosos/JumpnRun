70bc9c9407ae8658ae4dadb0e004524549f17e5d
AndyWieser <andywieser@web.de>
2014-05-10 20:46:32 +0200

    Anim

---
+++ JumpnRun/JumpnRun/JumpnRun/Draw/Animation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace JumpnRun
+{
+    class Animation
+    {
+        public Texture2D text;
+        public int part;
+
+
+        public Animation(Texture2D text)
+        {
+            this.text = text;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+        }
+    }
+}
