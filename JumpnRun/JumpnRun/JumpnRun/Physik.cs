93aa086f2ae01de6d189c06d0eb25d0e1e6f31e6
Kaitosos <kvnkersten@gmail.com>
2014-05-10 19:49:26 +0200

    started Physiks

---
+++ JumpnRun/JumpnRun/JumpnRun/Physik.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JumpnRun
+{
+    class Physik
+    {
+        private float gravity = -9.81f;
+        public float mass;
+        public float velocety;
+        public float jumpPower;
+        public bool standing;
+        public float movingUD;
+
+        public Physik(float mass, float velocety)
+        {
+            this.mass = mass;
+            this.velocety = velocety;
+            
+        this.jumpPower = 0;
+        this.standing = true;
+        this.movingUD = 0;
+        }
+
+        public float update()
+        {
+            return movingUD;
+        }
+    }
+}
